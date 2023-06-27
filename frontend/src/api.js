504f00df480a46832eae5bf0410fa00ee280df76
sinsong <literalkernel@outlook.com>
2023-06-27 22:43:36 +0800

    fix api base

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,9 @@
-const API_PKGLIST = 'https://bioarchlinux.org/api/pkglist.json'
-
 export
 async function requestPackageList () {
-  return await (await fetch('/api/pkglist.json')).json()
+  return await (await fetch(`${__API_BASE__}/api/pkglist.json`)).json()
 }
 
 export
 async function requestPackageInfo (pkg) {
-  return await (await fetch(`/api/${pkg}.json`)).json()
+  return await (await fetch(`${__API_BASE__}/api/${pkg}.json`)).json()
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import replace from '@rollup/plugin-replace'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({command}) => { return {
+  plugins: [
+    replace({
+      // production / development environment api base
+      __API_BASE__: command === 'build' ? JSON.stringify('https://bioarchlinux.org/')
+                                        : JSON.stringify(''),
+      preventAssignment: true,
+    }),
+    vue(),
+  ],
+  // for import alias
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -16,4 +26,4 @@ export default defineConfig({
       '/api': 'https://bioarchlinux.org'
     }
   }
-})
+}})
